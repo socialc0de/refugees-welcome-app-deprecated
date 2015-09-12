0bbc3190ea2aa7db0ece076fb1355763d854b868
Patrice Becker <patrice@5becker.de>
2015-09-12 17:59:53 +0200

    Expandable Listview... works

--- app/src/main/java/de/pajowu/donate/FAQCategoryFragment.java
+++ app/src/main/java/de/pajowu/donate/FAQCategoryFragment.java
@@ -1,17 +1,23 @@
 package de.pajowu.donate;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
+
+import java.util.ArrayList;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FAQCategoryFragment extends Fragment {
+    private ArrayList<String> parentItems = new ArrayList<String>();
+    private ArrayList<Object> childItems = new ArrayList<Object>();
 
 
     public FAQCategoryFragment() {
@@ -22,7 +28,54 @@ public class FAQCategoryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_faqcategory, container, false);
+        View v = inflater.inflate(R.layout.fragment_faqcategory, container, false);
+        ExpandableListView expandableList = (ExpandableListView) v.findViewById(R.id.list); // you can use (ExpandableListView) findViewById(R.id.list)
+
+        expandableList.setDividerHeight(2);
+        expandableList.setGroupIndicator(null);
+        expandableList.setClickable(true);
+
+        setGroupParents();
+        setChildData();
+
+        MyExpandableAdapter adapter = new MyExpandableAdapter(parentItems, childItems);
+
+        adapter.setInflater((LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE), getActivity());
+        expandableList.setAdapter(adapter);
+
+        return v;
+    }
+
+    public void setGroupParents() {
+        parentItems.add("Where may I get food for little money?");
+        parentItems.add("Does german food contain pig flesh?");
+        parentItems.add("Is german food expensive?");
+        parentItems.add("Where can I find typically Syrian Restaurants?");
+    }
+
+    public void setChildData() {
+
+        // Android
+        ArrayList<String> child = new ArrayList<String>();
+        child.add("Kindertafel");
+        childItems.add(child);
+
+        // Core Java
+        child = new ArrayList<String>();
+        child.add("Yes");
+        childItems.add(child);
+
+        // Desktop Java
+        child = new ArrayList<String>();
+        child.add("Unfortunately, yes.");
+
+        childItems.add(child);
+
+        // Enterprise Java
+        child = new ArrayList<String>();
+        child.add("EJB3");
+
+        childItems.add(child);
     }
 
 
--- app/src/main/java/de/pajowu/donate/NewOfferFragment.java
+++ app/src/main/java/de/pajowu/donate/NewOfferFragment.java
@@ -12,7 +12,6 @@ import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.util.Base64;

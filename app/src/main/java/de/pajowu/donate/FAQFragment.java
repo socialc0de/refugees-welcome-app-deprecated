17c689fa9cfcbdf4813837483cfcd753ad9a02f0
pajowu <github@ca.pajowu.de>
2015-09-12 14:40:24 +0200

    only use one kind of fragment

--- app/src/main/java/de/pajowu/donate/FAQFragment.java
+++ app/src/main/java/de/pajowu/donate/FAQFragment.java
@@ -1,7 +1,7 @@
 package de.pajowu.donate;
 
 
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;

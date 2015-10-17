7a13e673a6dde8bedb73ec331cc631dbf8fb1f4e
pajowu <github@ca.pajowu.de>
2015-10-17 16:54:29 +0200

    use com.github.socialc0de.gsw.android instead of de.pajowu.donate, it's no longer only pajowus project

---
+++ app/src/main/java/com/github/socialc0de/gsw/android/adapter/view/ContactViewAdapter.java
@@ -0,0 +1,48 @@
+package com.github.socialc0de.gsw.android.adapter.view;
+
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+import com.github.socialc0de.gsw.android.R;
+import com.github.socialc0de.gsw.android.models.ContactRow;
+import com.github.socialc0de.gsw.android.models.holder.ContactRowHolder;
+
+public class ContactViewAdapter extends RecyclerView.Adapter<ContactRowHolder> {
+    Context context;
+    ArrayList<ContactRow> itemsList;
+
+    public ContactViewAdapter(Context context, ArrayList<ContactRow> itemsList) {
+        this.context = context;
+        this.itemsList = itemsList;
+    }
+
+    @Override
+    public int getItemCount() {
+        if (itemsList == null) {
+            return 0;
+        } else {
+            return itemsList.size();
+        }
+    }
+
+    @Override
+    public ContactRowHolder onCreateViewHolder(ViewGroup viewGroup, int position) {
+        LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+        View view = inflater.inflate(R.layout.contact_row, null);
+        ContactRowHolder viewHolder = new ContactRowHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ContactRowHolder ContactRowHolder, int position) {
+        ContactRow items = itemsList.get(position);
+        ContactRowHolder.getTextView().setText(String.valueOf(items.getTitle()));
+        ContactRowHolder.getImageView().setBackgroundResource(items.getImgIcon());
+    }
+}
---
+++ app/src/main/java/com/github/socialc0de/gsw/android/adapter/view/ProfileViewAdapter.java
@@ -0,0 +1,48 @@
+package com.github.socialc0de.gsw.android.adapter.view;
+
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+import com.github.socialc0de.gsw.android.R;
+import com.github.socialc0de.gsw.android.models.ProfileRow;
+import com.github.socialc0de.gsw.android.models.holder.ProfileRowHolder;
+
+public class ProfileViewAdapter extends RecyclerView.Adapter<ProfileRowHolder> {
+    Context context;
+    ArrayList<ProfileRow> itemsList;
+
+    public ProfileViewAdapter(Context context, ArrayList<ProfileRow> itemsList) {
+        this.context = context;
+        this.itemsList = itemsList;
+    }
+
+    @Override
+    public int getItemCount() {
+        if (itemsList == null) {
+            return 0;
+        } else {
+            return itemsList.size();
+        }
+    }
+
+    @Override
+    public ProfileRowHolder onCreateViewHolder(ViewGroup viewGroup, int position) {
+        LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+        View view = inflater.inflate(R.layout.profile_row, null);
+        ProfileRowHolder viewHolder = new ProfileRowHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ProfileRowHolder ProfileRowHolder, int position) {
+        ProfileRow items = itemsList.get(position);
+        ProfileRowHolder.getTextView().setText(String.valueOf(items.getTitle()));
+        ProfileRowHolder.getImageView().setBackgroundResource(items.getImgIcon());
+    }
+}
